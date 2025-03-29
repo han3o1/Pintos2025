0a34def8f946d28737936ab03666ab355ef36aea
root <kimdoyi@naver.com>
2025-03-29 04:05:51 +0000

    Update alarm-negative.c

--- src/tests/threads/alarm-negative.c
+++ src/tests/threads/alarm-negative.c
@@ -6,10 +6,23 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "devices/timer.h"
+#include "threads/interrupt.h"  //Added
 
-void
-test_alarm_negative (void) 
-{
-  timer_sleep (-100);
-  pass ();
+/* Test for alarm clock */
+void test_alarm_clock(void) {
+  printf("Test started\n");
+  
+  /* Sleep for 10 ticks and wake up */
+  thread_sleep(10); // 10 틱 동안 잠들게 한다.
+  
+  /* After 10 ticks, the thread should wake up */
+  printf("Woke up after 10 ticks\n");
+  thread_yield(); // 기다린 후, 다른 스레드가 실행되도록 양보.
+  
+  printf("Test finished\n");
+}
+
+void run_alarm_clock_test(void) {
+  printf("Running alarm clock test...\n");
+  test_alarm_clock();
 }
