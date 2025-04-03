e52b11b071be86d399baf3dcb0645ca6283d8557
kimm00 <kimdoyi02@naver.com>
2025-04-03 11:11:45 +0000

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,17 +88,24 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    
+    int init_priority;
+    struct lock *wait_on_lock;
+    struct list donations;
+    struct list_elem donation_elem;
+    
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+
+    int64_t wakeup_tick;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
 #endif
 
-   int64_t wake_tick; /*the time when the threads wake up*/
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
   };
@@ -120,12 +127,6 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
-/*Add functions for sleep*/
-void thread_sleep(int64_t ticks);  //Sleep function
-void thread_unblock(struct thread *t); //Block release function
-void thread_block(void); //Thread block function
-void thread_tick(void);  //Timetic function
-
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
@@ -145,4 +146,13 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-#endif /* threads/thread.h */
+void thread_sleep(int64_t wakeup_tick);
+void thread_wakeup(int64_t current_tick);
+
+bool thread_priority_cmp(const struct list_elem *a, const struct list_elem *b, void *aux);
+
+void donate_priority(void);
+void remove_with_lock(struct lock *lock);
+void refresh_priority(void);
+
+#endif /* threads/thread.h */ 
